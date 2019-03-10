fb93a0a40517cf371fd84acc3f2ee457e73eafa6
Rastvor1tel <solvent71@yandex.ru>
2019-03-10 03:01:41 +0300

    Закончена основная верстка, начало адаптива

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 $(function () {
     var mySwiper = new Swiper ('.swiper-container', {
         loop: true,
+        navigation: {
+            nextEl: '.about-slider__arrows-item.right',
+            prevEl: '.about-slider__arrows-item.left',
+        },
     })
 })
