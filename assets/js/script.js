a71cbfc9adf3c17e527522a7e3b2966760d617f7
Rastvor1tel <solvent71@yandex.ru>
2019-03-12 03:36:47 +0300

    Закончен адаптив

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,26 @@ $(function () {
             prevEl: '.about-slider__arrows-item.left',
         },
     })
+    $('.callback-button').click(function(){
+        $('.callback-form').fadeIn();
+        $('.overlay').fadeIn();
+    });
+    $('.close-button, .overlay').click(function(){
+        $('.callback-form').fadeOut();
+        $('.overlay').fadeOut();
+    });
+    $('.callback-form form').submit(function(){
+        $.ajax({
+            url: "/assets/ajax-mail.php",
+            type: 'POST',
+            dataType: 'html',
+            data: $(this).serialize(),
+            context: document.body,
+            success: function(result){
+                console.log(result);
+            }
+        });
+
+        return false;
+    });
 })
